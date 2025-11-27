a0ad3048b161c80e23edbd4e64aa2620046d9300
YumNumm <admin@yumnumm.dev>
2025-11-28 01:08:03 +0900

    fix: mdsvexの設定を修正し、remarkPluginsを明示的に設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
+import { mdsvex, escapeSvelte } from "mdsvex";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePresetMinify from "rehype-preset-minify";
+import { createHighlighter } from "shiki";
+
+const highlighter = await createHighlighter({
+  themes: ["github-dark-high-contrast"],
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,21 +20,24 @@ const config = {
       smartypants: {
         dashes: "oldschool",
       },
+      remarkPlugins: [],
       rehypePlugins: [
         rehypeSlug,
         [
           rehypeAutolinkHeadings,
           {
             behavior: "append",
-            properties: {
-              className: ["heading-anchor"],
-              ariaHidden: true,
-              tabIndex: -1,
-            },
+            className: ["heading-anchor"],
           },
         ],
         rehypePresetMinify,
       ],
+      highlight: {
+        highlighter: async (code, lang = "text") => {
+          const html = escapeSvelte(highlighter.codeToHtml(code, lang));
+          return `{@html ${html}}`;
+        },
+      },
     }),
   ],
 
